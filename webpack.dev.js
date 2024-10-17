d1ee738c6815c2ca5df647ae47205e484814c79a
Johan <johanotr@gmail.com>
2024-10-17 18:47:55 -0400

    Init webpack

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+});
